6151dee18d8a5fb9310922915620a774822f1b88
Rodgers Opiyo <opiyorodgers030@gmail.com>
2023-11-23 12:29:05 +0300

    Completed labs

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+//const names = ["Guadalupe","Ollie","Aki"];
+function writeCards(names,event){
+ // const names = ["Guadalupe","Ollie","Aki"];
+  const message = [];
+  for (let i = 0; i < names.length ; i++){
+    const messages = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    message.push(messages);
+    //console.log(message);
+
+  }
+  return message;
+}
+  //writeCards(["Guadaulupe","Ollie","Aki"],"surprise");
+  console.log(writeCards(["Guadaulupe","Ollie","Aki"],"surprise"));
+  function countDown(){
+    let count = 0;
+    while (count < 11){
+      console.log(count++);
+    }
+  }
+    
